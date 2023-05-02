6163ddf8208969d6ccdc5d9ec86406c8d01a67e4
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-03 02:40:12 +0600

    learning generic type in TS

---
+++ src/module3/generic-type.ts
@@ -0,0 +1,19 @@
+const students1: string[] = ["Amir", "Kamir"];
+// const students1: Array<string> = ["Amir", "Kamir"];  //do same thing with Array angle bracket<>
+const students2: number[] = [1, 2, 3, 4, 5];
+// const students2: Array<number> = [1, 2, 3, 4, 5];
+
+const students3: Array<{ name: string; roll: number }> = [
+  {
+    name: "Dipta",
+    roll: 3,
+  },
+  {
+    name: "Keya",
+    roll: 4,
+  },
+];
+
+
+
+// Using Generic array
