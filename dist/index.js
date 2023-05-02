ea003587b5255ccaceb605fdfdd6f87ccc7e2b2f
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-02 21:59:40 +0600

    module 3 start

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 "use strict";
-const course = "Hello TS Developer";
-let fName = "Dipta Saha";
-
-
-console.log(fName);
+function generateAdder(a) {
+    return function (b) {
+        return a + b;
+    };
+}
+const arrTwo = generateAdder(2);
+console.log(arrTwo(3));
+console.log(arrTwo(5));
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-const course: string = "Hello TS Developer";
-let fName: string = "Dipta Saha";
 
-let roll: Number = 5000;
-
-
-console.log(fName)
-console.log(fName)
