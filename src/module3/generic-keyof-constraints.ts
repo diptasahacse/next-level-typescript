465e5a31ecabefa5b4ce7e2611e0fe30f32b0cfc
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-04 08:35:19 +0600

    complete generic keyof constrains concept

---
+++ src/module3/generic-keyof-constraints.ts
@@ -0,0 +1,36 @@
+type PersonType = {
+  name: string;
+  age: number;
+  address: string;
+};
+type newType = "name" | "age" | "address"; //Manual process
+
+// Means, Now we can only use or access name,age, address with newType by create a new Object
+// Example,
+const a1: newType = "address";
+const a2: newType = "age";
+const a3: newType = "name";
+// const a4:newType = "role" // we can't use any other property like this
+
+// same thing using keyOf
+type PersonType1 = {
+  name: string;
+  age: number;
+  address: string;
+};
+type newTypeUsingKeyOf = keyof PersonType1;
+
+const b1: newTypeUsingKeyOf = "address";
+const b2: newTypeUsingKeyOf = "name";
+const b3: newTypeUsingKeyOf = "age";
+// const b4:newTypeUsingKeyOf = "role"//We can't use any other property name like this
+
+// Let's see an example with extends and keyof concept,
+function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+  return obj[key];
+}
+// Here, Y receive a string, which extends a mandatory keys of X (name, age). means now Y is look like this = 'name' | 'age' 
+
+const property = getProperty({ name: "Dipta", age: 27 }, "age"); // we can not pass any other string like 'role', 'salary' except name, age. because Y extends all keys of X. and X has 'name' and 'age'
+
+console.log(property);
