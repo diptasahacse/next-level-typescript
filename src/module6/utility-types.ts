8539fb4f17bf59c927d038ecb121b1476866ef2a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2023-05-11 21:33:00 +0600

    utility type complete

--- src/module6/utility-types.ts
+++ src/module6/utility-types.ts
@@ -1,6 +1,6 @@
 // PICK
 
-interface Person {
+interface CustomPerson {
   name: string;
   email: string;
   contact: string;
@@ -11,13 +11,62 @@ interface Person {
 To do that thing, I have to use Pick this is a another generic
 
 */
-type Email = Pick<Person, "email">; // Here, I used email property with type is which Person used for email
-type Contact = Pick<Person, "email" | "contact">; // Here, I used email and contact property with type is which Person used for email and contact
-
+type Email = Pick<CustomPerson, "email">; // Here, I used email property with type is which Person used for email
+type Contact = Pick<CustomPerson, "email" | "contact">; // Here, I used email and contact property with type is which Person used for email and contact
 
 // OMIT   //It is alternative of Pick
 // If i want to use all property of Person except email property then,
-type user= Omit<Person, "email"> 
+type user = Omit<CustomPerson, "email">;
 
-// take all property wxcept email, contact,
-type user1= Omit<Person, "email"|"contact"> 
+// take all property except email, contact,
+type user1 = Omit<Person, "email" | "contact">;
+
+// Partial => To make all the property to be optional
+type myOptionalType = Partial<CustomPerson>;
+
+// Require => To make all the property to be Require
+interface CustomPerson1 {
+  name: string;
+  email: string;
+  contact: string;
+  age?: number;
+}
+
+type MyRequireType = Required<CustomPerson1>;
+
+// Readonly => to make the property readonly
+
+type MyData = Readonly<CustomPerson1>;
+
+// Record Type
+
+type mmyObjectType = {
+  a: string;
+  b: string;
+  c: string;
+};
+const obbj: mmyObjectType = {
+  a: "abc",
+  b: "def",
+  c: "ghi",
+};
+// Same thing using index signature
+type mmyObjectType1 = {
+  [key: string]: string;
+};
+
+// const mmyyObj:mmyObjectType1={
+//   a:'a',
+//   b:'bbbxx',
+//   c:'bxx',
+// }
+// type mmyObjectType1 = {
+//   [key: 'a'|'b'|'c']: string;  // I can not use like this
+// };
+// Let's use Record utility
+const mmmmyObj: Record<"name" | "role" | "address", string> = {
+  name: "Dipta",
+  role: "Developer",
+  address: "Khulna",
+  // age:455 // i can't use
+};
